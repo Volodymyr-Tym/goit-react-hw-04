e538fcc6d27deb56870088235590bb5d6a120ea7
Volodymyr-Tym <163361581+Volodymyr-Tym@users.noreply.github.com>
2024-10-12 18:49:16 +0100

    Init project
    Init project, add some files and install some libraries

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react';
+
+import Section from './components/Section/Section';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Section>
+        <h1>This is my React homework №4</h1>
+      </Section>
+    </>
+  );
+}
+
+export default App;
